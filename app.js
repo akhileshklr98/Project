5c0f9d7661952b2e70d713a3631228071fd468ba
akhileshklr98 <akhileshklr98@gmail.com>
2019-11-21 08:47:32 +0530

    21-11-2019

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/myApp'));
+app.use(express.static(__dirname + '/dist/MilmaApp'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname + 'dist/myApp/index.html'))
+    res.sendFile(path.join(__dirname + 'dist/MilmaApp/index.html'))
 });
 app.listen(process.env.PORT || 3000);
