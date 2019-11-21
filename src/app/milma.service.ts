c8ae5a43384604ffb4095ff6eadff059dd6765fe
akhileshklr98 <akhileshklr98@gmail.com>
2019-11-21 08:04:53 +0530

    21-11-2019

--- src/app/milma.service.ts
+++ src/app/milma.service.ts
@@ -9,27 +9,32 @@ export class MilmaService {
   constructor(public http: HttpClient) { }
 
   login(user){
-    console.log('service login')
-    return this.http.post("http://localhost:3000/login",{user});
+    return this.http.post("https://project-app-backend.herokuapp.com/login",{user});
+    // return this.http.post("http://localhost:3000/login",{user});
   }
 
   register(socity){
-    return this.http.post("http://localhost:3000/addSocity",{socity: socity});
+    return this.http.post("https://project-app-backend.herokuapp.com/addSocity",{socity: socity});
+    // return this.http.post("http://localhost:3000/addSocity",{socity: socity});
   }
 
   addsale(sale){
-    return this.http.post("http://localhost:3000/addSale",{sale: sale});
+    return this.http.post("https://project-app-backend.herokuapp.com/addSale",{sale: sale});
+    // return this.http.post("http://localhost:3000/addSale",{sale: sale});
   }
   
   buy(milma){
-    return this.http.post("http://localhost:3000/addFarmerDetails",{farmer: milma});
+    return this.http.post("https://project-app-backend.herokuapp.com/addFarmerDetails",{farmer: milma});
+    // return this.http.post("http://localhost:3000/addFarmerDetails",{farmer: milma});
   }
 
   getSocities(){
-    return this.http.get("http://localhost:3000/socities");
+    return this.http.get("https://project-app-backend.herokuapp.com/socities");
+    // return this.http.get("http://localhost:3000/socities");
   }
 
   getReport(){
-    return this.http.get("http://localhost:3000/report");
+    return this.http.get("https://project-app-backend.herokuapp.com/report");
+    // return this.http.get("http://localhost:3000/report");
   }
 }
